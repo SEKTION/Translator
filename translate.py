eea8f4e1798202211536cf43f587cb20765b1748
SEKTION <s3kt10n21@gmail.com>
2020-09-28 03:04:26 +0530

    Translator CLI

--- translate.py
+++ translate.py
@@ -1,85 +1,127 @@
+#!/usr/bin/env python3
 try:
+	
+	#Import necessary Modules
+	
 	from os import system
+	from time import sleep
 	import sys
+	import subprocess as sp
 
 	system('clear')
 
+	#Defining Variables
+
+	RED = sp.getoutput('tput setaf 9')
+	GREEN = sp.getoutput('tput setaf 10')
+	YELLOW = sp.getoutput('tput setaf 11')
+	BLUE = sp.getoutput('tput setaf 4')
+	CYAN = sp.getoutput('tput setaf 14')
+	WHITE = sp.getoutput('tput setaf 15')
+
+	#Check Internet Connectivity
+
 	connection = system('ping -c 2 goo.gl > /dev/null 2>&1')
 	if connection != 0:
-		sys.exit("Please check your Internet connection!")
+		print(RED)
+		sys.exit("Please check your Internet connection!\n")
 
+	#Check necessary tools and install if not already installed
+		
 	check = system('mpg123 --version > /dev/null 2>&1')
 	if check != 0:
-		print("Setting up... Please wait...", end="", flush=True)
-		system('sudo apt-get install mpg123 -y > /dev/null 2>&1')
+		print(GREEN)
+		print("Setting up... Please wait...", flush=True)
+		system('chmod +x trans && sudo apt-get install mpg123 -y > /dev/null 2>&1')
 		print("\nReady!\n")
+	
+
+	#Dictionary with language codes
 
 	lang_list = {}
 	with open('langs.txt','r') as file:
 		lang_list = dict([line.split('-') for line in file])
 
+	help = {
+		'1 to 5':'Self Explanatory',
+		'clear':'clear Screen',
+		'quit/q':'exit',
+		'help':'You are looking right at it baby!'
+	}
+
+	#Defining Functions
+
 	def print_list():
 		for i,j in enumerate(lang_list, start=1):
-				system("echo '\e[1;34m'")
-				print(str(i) + ' : ' + j, end="\n")
+				print(YELLOW)
+				print(str(i) + ' : ' + j, end="\n", flush=True)
+				sleep(0.1)
 
 	def translate():
+		source = 'English'
+		source = input("\nType the Source language [Default-English]: ").title()
+		if source.strip() == '':
+			source = 'English'
 		target = input("\nType the target language: ").title()
+		source = lang_list.get(source, 'Language Not Available!')
 		target = lang_list.get(target, 'Language Not Available!')
-		word = input("\nEnter a word or a sentence to translate: ")
-		output = './trans -b ' + "'" + word + "'" + ' -t ' + target
-		system('echo "\e[1;34m"')
+		output = './trans -b -shell ' + source.strip() + ':' + target.strip()
+		print(YELLOW)
 		system(output)
-		system('echo "\e[1;0m"')
 
 	def identify():
 		word = input("\nEnter a word or a sentence: ")
-		output = './trans -identify -no-auto ' + "'" + word + "' | head -n1"
-		system('echo "\e[1;34m"')
-		system(output)
-		system('echo "\e[1;0m"')
+		output = './trans -identify -no-auto ' + "'" + word + "' | grep -i name"
+		print(YELLOW)
+		lang_id = sp.getoutput(output).split()
+		print('\n' + lang_id[1] + '\n')
 
 	def pronounce():
-		word = input("\nEnter a word or a sentence: ")
-		output = './trans -speak -no-translate ' + "'" + word + "'"
-		system('echo "\e[1;34m"')
-		system(output)
-		system('echo "\e[1;0m"')
+		print(BLUE)
+		system('./trans -b -speak -shell')
 
-	system("echo '\n\t\t\e[1;31mWelcome to the Language Translator - A Python Script\n'")
-	system("echo '\t\t\t\e[1;32m[\tAuthor : SEKTION\t]'")
-	system("echo '\t\t\t\e[1;32m[\tVersion : 1.0\t\t]'")
+	print('\n\t\t\t' + RED + 'Welcome to the Language Translator\n')
+	print('\t\t\t' + GREEN + '[\tAuthor : SEKTION\t]')
+	print('\t\t\t' + GREEN + '[\tVersion : 1.0\t\t]\n')
 	
+	#Main-Menu which will run in loop until exit
+
 	while True:
-		system("echo '\n\t\t\t\t\e[1;36mSelect an Option!\n'")
-		opts = ["See list of Available Languages","Translate a word or a sentence","Identify a language","Listen to pronunciation","Help"]
+		print('\t\t\t\t' + CYAN + 'Select an Option!\n')
+		opts = ["List Available Languages","Translate words or sentences","Identify a language","Text-to-Speech","Help"]
 		for i,opt in enumerate(opts, start=1):
 			print(str(i) + ' : ' + opt)
 
-		choice = input("\n> ").strip()
+		cmd = input("\n> ").strip()
 
-		if choice == '1':
+		if cmd == '1':
 			system('clear')
 			print_list()
-			system('read _')	
-		elif choice == '2':
+				
+		elif cmd == '2':
 			translate()
-		elif choice == '3':
+		elif cmd == '3':
 			identify()
-		elif choice == '4':
+		elif cmd == '4':
 			pronounce()
-		elif choice == '5' or choice == 'help':
-			system("echo '\n\t\t\t1 to 5 : Self Explainatory\n\n\t\t\thelp : You are looking right at it baby!\n\n\t\t\tclear : clear Screen\n\n\t\t\tquit or q : exit\n\n'")
-			q = input("Type 'b' to go back: ").strip()
-			while q != 'b':
-				#print("Uh-Oh! Wrong Choice!")
-				q = input("Type 'b' to go back: ").strip()  
+		elif cmd == '5' or cmd == 'help':
+			for k,v in dict.items(help):
+				print(WHITE) 
+				print('\t\t\t' + k + '\t:\t' + v + '\n')
+				
+			q = ""
+			print(CYAN)
+			while q != 'b' or q == 'q':
+				q = input("Type 'b' to go back: ").strip()
+				if q == 'q':
+					sys.exit("\nGoodbye!\n")
 			
-		elif choice == 'clear':
+		elif cmd == 'clear':
 			system('clear')
-		elif choice == 'quit' or choice == 'q':
-			sys.exit("\nGoodbye!")
+		elif cmd == 'quit' or cmd == 'q':
+			sys.exit("\nGoodbye!\n")
 
+#Catch Keyboard Interrupt
 
 except KeyboardInterrupt:
-	print("\nNo need to be abrupt! You can always type 'q' to quit :)")		
+	print("\n\nNo need to be abrupt! You can always type 'q' to quit :)\n")		
